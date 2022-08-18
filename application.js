cc0ba6c381085c7c21be56ca4f0996dd3a25d84b
marcymallow <101074488+marcymallow@users.noreply.github.com>
2022-08-17 22:41:54 -0500

    Add files via upload

---
+++ application.js
@@ -0,0 +1,12 @@
+// Put your application javascript here
+const title = 'Bloom where you are planted.'
+
+const marqueeText = new Array(50).fill(title)
+
+const marquee = document.querySelector('h2')
+
+
+marquee.innerHTML = marqueeText
+
+
+
